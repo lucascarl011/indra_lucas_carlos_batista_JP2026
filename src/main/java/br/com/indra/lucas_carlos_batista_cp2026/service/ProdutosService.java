f6d69c7892ee7efe78fb0a3151ac955cc5b8e081
lucascarl011 <lucassscarlosss54@gmail.com>
2026-03-22 01:59:06 -0300

    Atualizações na classe ProdutosService

--- src/main/java/br/com/indra/lucas_carlos_batista_cp2026/service/ProdutosService.java
+++ src/main/java/br/com/indra/lucas_carlos_batista_cp2026/service/ProdutosService.java
@@ -2,6 +2,7 @@ package br.com.indra.lucas_carlos_batista_cp2026.service;
 
 import br.com.indra.lucas_carlos_batista_cp2026.model.HistoricoPreco;
 import br.com.indra.lucas_carlos_batista_cp2026.model.Produtos;
+import br.com.indra.lucas_carlos_batista_cp2026.repository.HistoricoPrecoRepository;
 import br.com.indra.lucas_carlos_batista_cp2026.repository.ProdutosRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,7 @@ import java.util.List;
 public class ProdutosService {
 
     private final ProdutosRepository produtosRepository;
+    private final HistoricoPrecoRepository historicoPrecoRepository;
 
     public List<Produtos> getAll(){
         return produtosRepository.findAll();
@@ -32,47 +34,23 @@ public class ProdutosService {
     }
 
     public Produtos getById(Long id){
-        return produtosRepository.findById(id).get();
+        return produtosRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Produto não encontrado"));
     }
 
     public Produtos atualizaPreco(Long id, BigDecimal preco) {
-//        Produtos produto = produtosRepository.findById(id).get();
+
         final var produto = produtosRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Produto não encontrado"));
-        produto.setPreco(preco);
-        /***
-         * Rastreabilidade
-         * 1 - Criar um log
-         * 2 - Adicionar em tabela historico de preços valores old e new
-         * para cada produto atualizado
-         * 3 - Antes de atualizar a tabela de produto, pegar o valor atual da tabela e adiconar
-         * na tabela historico
-         * 4 - Pegar novo valor da tabela e adicionar na tabela historico
-         * 5 - Sempre na tabela, adicionar novo registro após atualizar tabela de produto
-         * Estrutura da tabela historico de preços
-         * id
-         * id_produto
-         * preco_antigo
-         * preco_novo
-         * data_alteracao
-         */
+
         final var historico = new HistoricoPreco();
         historico.setPrecoAntigo(produto.getPreco());
         historico.setProdutos(produto);
         historico.setPrecoNovo(preco);
-        //Código abaixo pode ser substituido por @CreationTimestamp
-//        historico.setDataAlteracao(LocalDateTime.now());
         historicoPrecoRepository.save(historico);
-        return produtosRepository.saveAndFlush(produto);
 
-        //Exemplo de não se fazer por gerar retrabalho
-//        final var historicoNovo = historicoPrecoRepository.findById(historico.getId()).get();
-//        historicoNovo.setPrecoNovo(preco);
-//        historicoPrecoRepository.save(historicoNovo);
-        /**
-         * get na tabela produtos para novo preço
-         */
-//        return produto;
+        produto.setPreco(preco);
+        return produtosRepository.saveAndFlush(produto);
     }
 
 
