312476e5071e317d8114fbd73112ced743f6cdb4
lucascarl011 <lucassscarlosss54@gmail.com>
2026-03-23 20:21:24 -0300

    feat: entidade ItemCarrinho implementada

--- src/main/java/br/com/indra/lucas_carlos_batista_cp2026/model/ItemCarrinho.java
+++ src/main/java/br/com/indra/lucas_carlos_batista_cp2026/model/ItemCarrinho.java
@@ -1,4 +1,39 @@
 package br.com.indra.lucas_carlos_batista_cp2026.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "item_carrinho")
 public class ItemCarrinho {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "carrinho_id", nullable = false)
+    private Carrinho carrinho;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id", nullable = false)
+    private Produtos produto;
+
+    @Column(name = "quantidade", nullable = false)
+    private Integer quantidade;
+
+    @Column(name = "preco_registrado", nullable = false)
+    private BigDecimal precoRegistrado;
+
+    @Column(name = "ativo")
+    private Boolean ativo = true;
 }
