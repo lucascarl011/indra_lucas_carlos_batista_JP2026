911816221f5154a7073f75598807f414dcc1c3cd
lucascarl011 <lucassscarlosss54@gmail.com>
2026-03-24 22:19:23 -0300

    feat: implementacao da entidade InventarioTransacao finalizada

--- src/main/java/br/com/indra/lucas_carlos_batista_cp2026/model/InventarioTransacao.java
+++ src/main/java/br/com/indra/lucas_carlos_batista_cp2026/model/InventarioTransacao.java
@@ -1,4 +1,41 @@
 package br.com.indra.lucas_carlos_batista_cp2026.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "inventario_transacao")
 public class InventarioTransacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id", nullable = false)
+    private Produtos produto;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo", nullable = false)
+    private TipoTransacao tipo;
+
+    @Column(name = "quantidade", nullable = false)
+    private Integer quantidade;
+
+    @Column(name = "motivo")
+    private String motivo;
+
+    @CreationTimestamp
+    @Column(name = "data_transacao", updatable = false)
+    private LocalDateTime dataTransacao;
 }
