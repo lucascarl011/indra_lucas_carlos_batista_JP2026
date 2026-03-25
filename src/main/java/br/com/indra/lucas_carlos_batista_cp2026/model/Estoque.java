a4f8140d618a998f42459956553b6a2e56a1e353
lucascarl011 <lucassscarlosss54@gmail.com>
2026-03-24 22:14:00 -0300

    feat: implmentação da entidade Estoque finalizada

--- src/main/java/br/com/indra/lucas_carlos_batista_cp2026/model/Estoque.java
+++ src/main/java/br/com/indra/lucas_carlos_batista_cp2026/model/Estoque.java
@@ -1,7 +1,6 @@
 package br.com.indra.lucas_carlos_batista_cp2026.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,5 +14,21 @@ import lombok.Setter;
 @Table(name = "estoque")
 public class Estoque {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "produto_id", nullable = false)
+    private Produtos produto;
+
+    @Column(name = "quantidade", nullable = false)
+    private Integer quantidade = 0;
+
+    @Column(name = "quantidade_minima")
+    private Integer quantidadeMinima = 0;
+
+    @Column(name = "estoque_baixo")
+    private Boolean estoqueBaixo = false;
 
 }
