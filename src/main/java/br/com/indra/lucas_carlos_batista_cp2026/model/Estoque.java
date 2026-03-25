8d82fc93b9dada740a14b7cb700d049b4781c86d
lucascarl011 <lucassscarlosss54@gmail.com>
2026-03-24 22:07:49 -0300

    feat: impementacao da entidade Estoque

---
+++ src/main/java/br/com/indra/lucas_carlos_batista_cp2026/model/Estoque.java
@@ -0,0 +1,19 @@
+package br.com.indra.lucas_carlos_batista_cp2026.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "estoque")
+public class Estoque {
+
+
+}
