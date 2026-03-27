5123de7445daaab7f0f74450ac03d9b4a9321418
lucascarl011 <lucassscarlosss54@gmail.com>
2026-03-27 20:34:47 -0300

    feat: add ItemPedidoRepository

---
+++ src/main/java/br/com/indra/lucas_carlos_batista_cp2026/repository/ItemPedidoRepository.java
@@ -0,0 +1,7 @@
+package br.com.indra.lucas_carlos_batista_cp2026.repository;
+
+import br.com.indra.lucas_carlos_batista_cp2026.model.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class ItemPedidoRepository extends JpaRepository<ItemPedido, Long> {
+}
