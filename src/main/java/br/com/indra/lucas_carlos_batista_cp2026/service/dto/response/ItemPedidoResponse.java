a84bdd993fb1848bef6a68224c4552d1734f5977
lucascarl011 <lucassscarlosss54@gmail.com>
2026-03-27 20:41:20 -0300

    feat: add ItemPedidoResponse

---
+++ src/main/java/br/com/indra/lucas_carlos_batista_cp2026/service/dto/response/ItemPedidoResponse.java
@@ -0,0 +1,13 @@
+package br.com.indra.lucas_carlos_batista_cp2026.service.dto.response;
+
+import java.math.BigDecimal;
+
+public record ItemPedidoResponse(
+
+        Long id,
+        Long produtoId,
+        String produtoNome,
+        Integer quantidade,
+        BigDecimal precoRegistrado,
+        BigDecimal subtotal
+) {}
