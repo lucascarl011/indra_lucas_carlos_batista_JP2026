8da420b143d6aa1b344aec2a99ddcffe4d630322
lucascarl011 <lucassscarlosss54@gmail.com>
2026-03-27 20:31:46 -0300

    feat: add entidades Pedido e ItemPedido

---
+++ src/main/java/br/com/indra/lucas_carlos_batista_cp2026/model/ItemPedido.java
@@ -0,0 +1,36 @@
+package br.com.indra.lucas_carlos_batista_cp2026.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "item_pedido")
+public class ItemPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id", nullable = false)
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id", nullable = false)
+    private Produtos produto;
+
+    @Column(name = "quantidade", nullable = false)
+    private Integer quantidade;
+
+    @Column(name = "preco_registrado", nullable = false)
+    private BigDecimal precoRegistrado;
+}
---
+++ src/main/java/br/com/indra/lucas_carlos_batista_cp2026/model/Pedido.java
@@ -0,0 +1,52 @@
+package br.com.indra.lucas_carlos_batista_cp2026.model;
+
+import br.com.indra.lucas_carlos_batista_cp2026.model.enuns.StatusPedido;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "pedido")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "usuario_id", nullable = false)
+    private Long usuarioId;
+
+    @Column(name = "total", nullable = false)
+    private BigDecimal total;
+
+    @Column(name = "desconto")
+    private BigDecimal desconto = BigDecimal.ZERO;
+
+    @Column(name = "frete")
+    private BigDecimal frete = BigDecimal.ZERO;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private StatusPedido status = StatusPedido.CRIADO;
+
+    @CreationTimestamp
+    @Column(name = "criado_em", updatable = false)
+    private LocalDateTime criadoEm;
+
+    @Column(name = "endereco")
+    private String endereco;
+
+    @OneToMany(mappedBy = "pedido", fetch = FetchType.EAGER)
+    private List<ItemPedido> itens = new ArrayList<>();
+}
