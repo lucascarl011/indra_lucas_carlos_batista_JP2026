1d4c63ab16dda38f8609e26a938d9f71e11b4a78
lucascarl011 <lucassscarlosss54@gmail.com>
2026-03-27 19:19:38 -0300

    feat: add CarrinhoResponse

---
+++ src/main/java/br/com/indra/lucas_carlos_batista_cp2026/service/dto/response/CarrinhoResponse.java
@@ -0,0 +1,13 @@
+package br.com.indra.lucas_carlos_batista_cp2026.service.dto.response;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record CarrinhoResponse(
+
+        Long id,
+        Long usuarioId,
+        List<ItemCarrinhoResponse> itens,
+        BigDecimal total,
+        Boolean ativo
+) {}
